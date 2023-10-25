1565e653c6f71e33df8485e9e40f039e7bf134fe
Santiago <santisayago@gmail.com>
2023-10-25 09:24:40 -0300

    Funciona Server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (reg, res) => {
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+app.listen(PORT, () => {
+    //if (err) console.log(err)
+    console.log('Server is running on port http://localhost:${PORT}');
+});
