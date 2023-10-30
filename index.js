9d269ae694964fba9c0b4d15542e129c430bc4db
Santiago <santisayago@gmail.com>
2023-10-30 09:25:04 -0300

    avances en proyecto

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//middelware: funciones que estan entre la petivion y la respuesta
+const miMiddelware = (req, res, next) => {
+    console.log('hola soy un middleware');
+    console.log(__dirname);
+}
+
+app.use(miMiddelware);
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (reg, res) => {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile('index.html');
+});
+
+app.get('/ofertas', (reg, res) => {
+    res.sendFile('ofertas.html');
 });
 
 app.listen(PORT, () => {
-    //if (err) console.log(err)
+    if (err) console.log(err);
     console.log('Server is running on port http://localhost:${PORT}');
 });
